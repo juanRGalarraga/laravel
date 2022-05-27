36b7490d51b66cb0b34d96354fdb106fbfa35855
Juan <juan_dlp2@outlook.com>
2022-05-27 20:09:57 -0300

    First commit laravel 9

---
+++ laravel9/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+use PhpParser\Node\Arg;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::orderBy('id', 'desc')->get();
+        return $articles;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {   
+        Article::create($request->all());
+        return Article::all()->last();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {   
+        Article::findOrFail($request->id);
+        return Article::query()->get()->where('id', '=', $request->id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $article = new Article();
+
+        Article::findOrFail($request->id);
+        $article->update($request->all());
+
+        return $article->all()->last();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        Article::findOrFail($request->id);
+        Article::destroy($request->id);
+    }
+}
